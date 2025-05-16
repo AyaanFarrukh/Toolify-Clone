1cf3250a516b350455c876b13cca8f8512ed29a7
Ayaan Farrukh <ayaanfarrukhraza@gmail.com>
2025-05-16 17:43:09 +0500

    responsive and added some effects

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,30 @@ import { Footer } from "./Components/Footer";
 import { Header } from "./Components/header";
 import { MainContent } from "./Components/main";
 import { MainArea } from "./Components/MainArea";
+import "../src/globals.css"
+import { useEffect,useRef } from "react";
 
 export function App() {
+   const cursorLightRef = useRef(null);
+   useEffect(() => {
+      const light = cursorLightRef.current;
+  
+      const handleMouseMove = (e) => {
+         if(light) {
+            light.style.transform = `translate(${e.clientX - 100}px, ${e.clientY - 100}px)`;
+         }
+      };
+  
+      document.addEventListener("mousemove",handleMouseMove);
+  
+      return () => {
+        document.removeEventListener("mousemove", handleMouseMove);
+      };
+    }, []);
    return (
    <div style={{display: "flex", flexDirection: "column"
    }}>
+        <div ref={cursorLightRef} id="cursor-light"></div>
    <Header/>
    <EntryBox />
    <MainArea/>
--- src/Components/mainboxes.jsx
+++ src/Components/mainboxes.jsx
@@ -1,7 +1,29 @@
 import "./main.css"
+import { useRef, useState,useEffect } from "react";
 
 export function MainBoxes({title,description}) {
-       return <div className="box">
+     const ref = useRef(null);
+     const [isVisible,setIsVisible] = useState(false);
+
+     useEffect(() => {
+          const observer = new IntersectionObserver(
+            ([entry]) => {
+            setIsVisible(entry.isIntersecting);
+            },
+            {
+              threshold: 0.1,
+            }
+          );
+      
+          if (ref.current) {
+            observer.observe(ref.current);
+          }
+      
+          return () => {
+            if (ref.current) observer.unobserve(ref.current);
+          };
+        }, []);
+       return <div className={`box fade-in ${isVisible ? "visible" : ""}`} ref={ref}>
             <h3 className="tool-title">{title}</h3>
             <p className="tool-description">{description}</p>
        </div>
